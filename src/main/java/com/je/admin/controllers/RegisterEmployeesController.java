455946922f8e78ea3df6ae2a7bd435a0d24c67e0
MariaAngeles <MariAngeles@Mariangeles>
2019-10-14 12:36:28 +0200

    Arreglo registro de empleados

--- src/main/java/com/je/admin/controllers/RegisterEmployeesController.java
+++ src/main/java/com/je/admin/controllers/RegisterEmployeesController.java
@@ -14,9 +14,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.je.admin.forms.AdminForm;
@@ -48,7 +49,7 @@ public class RegisterEmployeesController {
 		return model;
 	}
 
-	@GetMapping("/registeremployees")
+	@PostMapping("/registeremployees")
 	public ModelAndView registeremployees(@ModelAttribute(ConstantsJsp.FORMSEARCH) SearchForm form,
 			BindingResult result) {
 		ModelAndView model = new ModelAndView();
