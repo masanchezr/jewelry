2795b322592ad80ebc408e06dc21d59c95d2f85d
mangeles <mangeles@localhost>
2020-12-01 21:26:59 +0100

    La suma de los arreglos se toma el carry date no el creationdate

--- src/main/java/com/je/dbaccess/repositories/AdjustmentRepository.java
+++ src/main/java/com/je/dbaccess/repositories/AdjustmentRepository.java
@@ -41,11 +41,11 @@ public interface AdjustmentRepository extends CrudRepository<AdjustmentEntity, L
 	List<AdjustmentEntity> findByCarrydateBetweenAndPayment(@Temporal(TemporalType.DATE) Date from,
 			@Temporal(TemporalType.DATE) Date until, PaymentEntity payment);
 
-	@Query("select sum(a.amountwork) from AdjustmentEntity a where a.creationdate>=:from and a.creationdate<=:until and a.place=:place")
+	@Query("select sum(a.amountwork) from AdjustmentEntity a where a.carrydate>=:from and a.carrydate<=:until and a.place=:place")
 	BigDecimal sumAmountworkByCreationdateAndPlace(@Param("from") @Temporal(TemporalType.DATE) Date from,
 			@Param("until") @Temporal(TemporalType.DATE) Date until, @Param(Constants.PLACE) PlaceEntity placeEntity);
 
-	@Query("select sum(a.amount) from AdjustmentEntity a where a.creationdate>=:from and a.creationdate<=:until and a.place=:place")
+	@Query("select sum(a.amount) from AdjustmentEntity a where a.carrydate>=:from and a.carrydate<=:until and a.place=:place")
 	BigDecimal sumAmountByCreationdateAndPlace(@Param("from") @Temporal(TemporalType.DATE) Date from,
 			@Param("until") @Temporal(TemporalType.DATE) Date until, @Param(Constants.PLACE) PlaceEntity placeEntity);
 
