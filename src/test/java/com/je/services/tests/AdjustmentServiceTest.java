8a88455cc90bb9d909aa1c3bba79ec932dd67b0b
mangeles <mangeles@localhost>
2020-12-01 21:14:35 +0100

    Subo un test

--- src/test/java/com/je/services/tests/AdjustmentServiceTest.java
+++ src/test/java/com/je/services/tests/AdjustmentServiceTest.java
@@ -1,24 +1,39 @@
 package com.je.services.tests;
 
+import java.util.Date;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.je.dbaccess.entities.PlaceEntity;
 import com.je.services.adjustments.AdjustmentService;
+import com.je.utils.date.DateUtil;
 
 import junit.framework.Assert;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = { "classpath*:application-context-test.xml" })
-public class AdjustmentServiceTest {
+class AdjustmentServiceTest {
 
 	@Autowired
 	private AdjustmentService adjustmentservice;
 
 	@Test
-	public void findByIdTest() {
+	void findByIdTest() {
 		Assert.assertNotNull(adjustmentservice.findById(23L));
+	}
+
+	@Test
+	void sumAdjustmentByDatesTest() {
+		PlaceEntity place = new PlaceEntity();
+		String sfrom = "20/01/2019";
+		String suntil = "01/12/2020";
+		place.setIdplace(24002L);
+		Date from = DateUtil.getDate(sfrom);
+		Date until = DateUtil.getDate(suntil);
+		Assert.assertNotNull(adjustmentservice.sumAdjustmentByDates(from, until, place));
 	}
 }
