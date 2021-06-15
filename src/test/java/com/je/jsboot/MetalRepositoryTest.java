d066b5222671840600b862cbe2f049a270919973
ATM <ATM@DESKTOP-VAOO7RR>
2021-06-15 20:04:39 +0200

    Refactor Tests

--- src/test/java/com/je/jsboot/MetalRepositoryTest.java
+++ src/test/java/com/je/jsboot/MetalRepositoryTest.java
@@ -1,13 +1,10 @@
 package com.je.jsboot;
 
-import java.util.Date;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.je.jsboot.dbaccess.entities.MetalEntity;
 import com.je.jsboot.dbaccess.repositories.MetalRepository;
 
 /**
--- src/test/java/com/je/jsboot/PawnServiceTest.java
+++ src/test/java/com/je/jsboot/PawnServiceTest.java
@@ -1,18 +1,11 @@
 package com.je.jsboot;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.je.jsboot.dbaccess.entities.MetalEntity;
-import com.je.jsboot.dbaccess.entities.ObjectPawnEntity;
 import com.je.jsboot.dbaccess.entities.PlaceEntity;
-import com.je.jsboot.services.pawns.NewPawn;
 import com.je.jsboot.services.pawns.Pawn;
 import com.je.jsboot.services.pawns.PawnService;
 
