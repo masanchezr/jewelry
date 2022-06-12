dbdc03c584043d55d47cb24ea2d9660ce311f8aa
masanchezr <webmariangeless@hotmail.com>
2022-06-12 17:36:48 +0200

    Update JsbootSecurityConfig.java
    Debido a la subida de spring boot 2.7

--- src/main/java/com/je/jsboot/JsbootSecurityConfig.java
+++ src/main/java/com/je/jsboot/JsbootSecurityConfig.java
@@ -1,14 +1,15 @@
 package com.je.jsboot;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.je.jsboot.services.users.UserDetailServiceImpl;
 
@@ -22,32 +23,33 @@ public class JsbootSecurityConfig {
 
 	@Configuration
 	@Order(1)
-	public static class WorkshopSecurityConfig extends WebSecurityConfigurerAdapter {
+	public static class WorkshopSecurityConfig {
 		private static final String ROLEJORGE = "JORGE";
 		private static final String LOGINURL = "/workshop/login";
 		private static final String ADMINURL = "/workshop/admin";
 
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
+		@Bean
+		public SecurityFilterChain workshopFilterChain(HttpSecurity http) throws Exception {
 			http.antMatcher("/workshop/**").authorizeRequests().anyRequest().hasRole(ROLEJORGE).and().formLogin()
 					.loginPage(LOGINURL).permitAll().defaultSuccessUrl(ADMINURL, true).failureForwardUrl("/403wks")
 					.and().exceptionHandling().accessDeniedPage("/403wks").and().logout()
 					.logoutUrl("/workshop/j_spring_security_logout").logoutSuccessUrl(LOGINURL)
 					.invalidateHttpSession(true);
+			return http.build();
 		}
 	}
 
 	@Configuration
 	@Order(2)
-	public static class EmployeeSecurityConfig extends WebSecurityConfigurerAdapter {
+	public static class EmployeeSecurityConfig {
 		private static final String ROLEUSER = "USER";
 		private static final String ROLEAR = "AR";
 		private static final String ROLENA = "NA";
 		private static final String LOGINURL = "/employee/login";
 		private static final String ADMINURL = "/employee/admin";
 
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
+		@Bean
+		public SecurityFilterChain employeeFilterChain(HttpSecurity http) throws Exception {
 			http.antMatcher("/employee/**").authorizeRequests()
 					.antMatchers(ADMINURL, "/employee/savePawn", "/employee/newPawn", "/employee/daily",
 							"/employee/newsale", "/employee/newshopping", "/employee/removesale",
@@ -66,18 +68,19 @@ public class JsbootSecurityConfig {
 					.permitAll().defaultSuccessUrl(ADMINURL, true).failureForwardUrl("/403").and().exceptionHandling()
 					.accessDeniedPage("/403").and().logout().logoutUrl("/employee/j_spring_security_logout")
 					.logoutSuccessUrl(LOGINURL).invalidateHttpSession(true);
+			return http.build();
 		}
 	}
 
 	@Configuration
 	@Order(3)
-	public static class AdminSecurityConfig extends WebSecurityConfigurerAdapter {
+	public static class AdminSecurityConfig {
 		private static final String ROLEADMIN = "ADMIN";
 		private static final String LOGINURL = "/login";
 		private static final String ADMINURL = "/admin";
 
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
+		@Bean
+		public SecurityFilterChain adminFilterChain(HttpSecurity http) throws Exception {
 			http.authorizeRequests()
 					.antMatchers(ADMINURL, "/newJewel", "/saveJewel", "/updateJewels", "/searchJewels", "/newCategory",
 							"/newPayment", "/newPawn", "/savePayment", "/error", "/searchclients", "/disablejewel",
@@ -95,15 +98,16 @@ public class JsbootSecurityConfig {
 					.defaultSuccessUrl(ADMINURL, true).failureForwardUrl("/403admin").and().exceptionHandling()
 					.accessDeniedPage("/403admin").and().logout().logoutUrl("/j_spring_security_logout")
 					.logoutSuccessUrl(LOGINURL).invalidateHttpSession(true);
+			return http.build();
 		}
 	}
 
 	@Configuration
 	@Order(4)
-	public static class AllSecurityConfig extends WebSecurityConfigurerAdapter {
-		@Override
-		public void configure(WebSecurity web) throws Exception {
-			web.ignoring().antMatchers(HttpMethod.GET, resources);
+	public static class AllSecurityConfig {
+		@Bean
+		public WebSecurityCustomizer webSecurityCustomiz() throws Exception {
+			return (web) -> web.ignoring().antMatchers(HttpMethod.GET, resources);
 		}
 	}
 
